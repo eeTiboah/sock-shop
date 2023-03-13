3b3ee374c4873ceddd4359462af082c9db443ab9
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-13 20:10:23 +0000

    get demo.yml file

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,7 +1,7 @@
 
 
 locals {
-  demo_manifests = yamldecode(file("${path.module}/../deploy/kubernetes/complete-demo.yaml"))
+  demo_manifests = yamldecode(file("../deploy/kubernetes/complete-demo.yaml"))
 }
 
 resource "kubernetes_manifest" "demo" {
