a63909d5cc614ba2a0992bae1de86f8d60a39f5d
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-13 20:05:52 +0000

    add kubernetes manifest

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,7 +1,7 @@
 
 
 locals {
-  demo_manifests = file("${path.module}/../deploy/kubernetes/complete-demo.yaml")
+  demo_manifests = yamldecode(file("${path.module}/../deploy/kubernetes/complete-demo.yaml"))
 }
 
 resource "kubernetes_manifest" "demo" {
