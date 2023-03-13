e00b8071800dae3f1904833fc630d65ab7ee9a1b
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-13 20:59:54 +0000

    create local file

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,15 +1,10 @@
 
-resource "null_resource" "copy_files" {
-  provisioner "local-exec" {
-    command = "cp -r ../deploy/kubernetes/manifests/ ${path.module}/kubernetes/"
-  }
-}
-
-
-locals {
-    namespace = yamldecode(file("${path.module}/kubernetes/00-sock-shop.yaml"))
-}
 
 resource "kubernetes_manifest" "namespace" {
-    manifest = local.namespace
+    manifest = <<EOF
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+    name: sock-shop
+EOF
 }
