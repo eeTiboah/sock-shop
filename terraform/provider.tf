f935e40fd3a7a29148e19fd808e7ce3737289ed7
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-13 21:10:37 +0000

    create local file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,6 +14,7 @@ terraform {
 provider "kubernetes" {
   host                   = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+  config_context_cluster = aws_eks_cluster.demo.arn
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
