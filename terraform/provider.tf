5f3c9a338d9dffa3837ed7b18111a0e935d7c602
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-13 19:49:02 +0000

    add kubernetes provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,14 +7,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.10"
     }
-    # kubernetes = {
-    #   host                   = var.cluster_endpoint
-    #   cluster_ca_certificate = base64decode(var.cluster_ca_cert)
-    #   exec = {
-    #     api_version = "client.authentication.k8s.io/v1beta1"
-    #     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    #     command     = "aws"
-    #   }
-    # }
+    kubernetes = {
+      host                   = var.cluster_endpoint
+      cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+      exec = {
+        api_version = "client.authentication.k8s.io/v1beta1"
+        args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+        command     = "aws"
+      }
+    }
   }
 }
