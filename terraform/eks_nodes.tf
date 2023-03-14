3966b037965d746d10e100c749e6c1bc34172844
Emmanuel Tiboah <etiboah@gmail.com>
2023-03-14 09:11:52 +0000

    edit terraform yaml file to include eks deployment

--- terraform/eks_nodes.tf
+++ terraform/eks_nodes.tf
@@ -41,14 +41,14 @@ resource "aws_eks_node_group" "private_nodes" {
   node_role_arn   = aws_iam_role.nodes.arn
 
   # Single subnet to avoid data transfer charges while testing.
-  
+
   subnet_ids = [
     aws_subnet.private_us_east_1a.id,
     aws_subnet.private_us_east_1b.id
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3a.xlarge"]
+  instance_types = ["t2.micro"]
 
   scaling_config {
     desired_size = 2
